a6d8182f8c83240aa8d6151ef8b646848fe5c214
lixuhui123 <2871726754@qq.com>
2019-11-27 23:53:35 +0800

    一点更新

--- 10_13/find.cpp
+++ 10_13/find.cpp
@@ -1,10 +1,57 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//	string str("lixuhui-sada?dewdddw");
+//	 
+//	cout << i << endl;
+//	system("pause"); 
+//	return 0;
+//}
+
+ 
+
 #include <iostream>
 #include <string>
+
 using namespace std;
+
 int main()
 {
-	string str("lixuhui-sada?dewdddw");
-	int i=str.find('-');
-	system("pause"); 
+	string str1, str2;
+
+	while (cin >> str1 >> str2)
+	{
+		int i = 0, j = 0;
+		int b = i - 1;
+
+		int flag = 1;
+		for (; j < str2.length(); j++)
+		{
+			if (str2[j] == str1[i] || str1[i] == '?')
+			{
+				i++;
+				continue;
+			}
+			else if (str1[i] == '*')
+			{
+				i++;
+				continue;
+			}
+			else if (b >= 0 && str1[b] == '*')
+			{
+				continue;
+			}
+			else
+			{
+				cout << "false" << endl;
+				flag = 0;
+				break;
+			}
+		}
+		if (flag == 1)
+			cout << "true" << endl;
+	}
 	return 0;
 }
